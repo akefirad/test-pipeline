39733f5384fc516dfc518d72573f51a47ecdf8a0
akefirad <akefirad@gmail.com>
2022-04-24 14:42:35 +0200

    GH-0 add stacsasdfad

--- lib/test-pipeline-stack.ts
+++ lib/test-pipeline-stack.ts
@@ -1,16 +1,52 @@
-import { Stack, StackProps } from 'aws-cdk-lib';
-import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import { Stack, StackProps, Stage, StageProps } from "aws-cdk-lib";
+import { Function, InlineCode, Runtime } from "aws-cdk-lib/aws-lambda";
+import * as pipelines from "aws-cdk-lib/pipelines";
+import { Construct } from "constructs";
 
-export class TestPipelineStack extends Stack {
+export class MyPipelineStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    const pipeline = new pipelines.CodePipeline(this, "Pipeline1", {
+      pipelineName: "RadTestPipeline",
+      crossAccountKeys: true,
+      synth: new pipelines.ShellStep("Synth", {
+        input: pipelines.CodePipelineSource.gitHub(
+          "akefirad/test-pipeline",
+          "main"
+        ),
+        commands: ["npm ci", "npm run build", "npx cdk synth"],
+      }),
+    });
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'TestPipelineQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    pipeline.addStage(
+      new MyPipelineStage(this, "Stage1", {
+        env: {
+          account: "388531472195",
+          region: "eu-central-1",
+        },
+      })
+    );
+  }
+}
+
+class MyPipelineStage extends Stage {
+  constructor(scope: Construct, id: string, props?: StageProps) {
+    super(scope, id, props);
+
+    new MyLambdaStack(this, "Lambda1", {});
+    // new MyLambdaStack(this, "Lambda2", {});
+  }
+}
+
+export class MyLambdaStack extends Stack {
+  constructor(scope: Construct, id: string, props?: StackProps) {
+    super(scope, id, props);
+
+    new Function(this, "LambdaFunction", {
+      runtime: Runtime.NODEJS_14_X,
+      handler: "index.handler",
+      code: new InlineCode('exports.handler = _ => "Hello, CDK";'),
+    });
   }
 }
